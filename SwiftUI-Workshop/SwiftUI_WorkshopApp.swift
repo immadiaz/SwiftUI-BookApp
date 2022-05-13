0f962065457cc965514582030004442633a99ed0
Immanuel Salazar Diaz <immanuel.salazar@wizeline.com>
2022-05-13 15:26:23 -0500

    Initial Commit

---
+++ SwiftUI-Workshop/SwiftUI_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WorkshopApp.swift
+//  SwiftUI-Workshop
+//
+//  Created by Immanuel Salazar on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
